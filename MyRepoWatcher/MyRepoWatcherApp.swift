36a62a6f351012852cb168c20343b1ba32a9ee16
Mark J Perryman <tazmancoder@gmail.com>
2024-06-17 13:06:05 -0600

    Initial Commit

---
+++ MyRepoWatcher/MyRepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRepoWatcherApp.swift
+//  MyRepoWatcher
+//
+//  Created by Mark Perryman on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MyRepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
